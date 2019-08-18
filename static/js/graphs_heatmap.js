fd3e2d14b12effbaa3ba148d83089fab9b57e236
Yvette Smith <yvette@boesman.nl>
2019-08-18 19:19:24 +0200

    time dimension to heatmap

--- static/js/graphs_heatmap.js
+++ static/js/graphs_heatmap.js
@@ -6,22 +6,50 @@ $(document).ready(function() {
 
 function parseKnmiRow(d) {
     return {
-        YYYYMMDD: d3.timeParse("%Y-%m-%d"),
+        date: d3.timeParse("%Y%m%d")(d["YYYYMMDD"]),
         "LON(east)": parseFloat(d["LON(east)"]),
         "LAT(north)": parseFloat(d["LAT(north)"]),
         sunShineHour: parseFloat(d['SQ']),
         sunShineduration: parseFloat(d['SP']),
         globalRadiationJcm2: parseFloat(d['Q']),
+        d:d,
     };
 }
 
 function makeGrGraphs(error, knmiData) {
     var ndx = crossfilter(knmiData);
     var allDim = ndx.dimension(function(d) { return d; });
+    var dateDim = ndx.dimension(dc.pluck('date'));
     var radiationDim = ndx.dimension(dc.pluck("globalRadiationJcm2"));
-    var maxRadiation = radiationDim.top(1)[0]["globalRadiationJcm2"] * 365;
+    var maxRadiation = radiationDim.top(1)[0]["globalRadiationJcm2"];
+    
+    var minDate = dateDim.bottom(1)[0]["date"];
+    var maxDate = dateDim.top(1)[0]["date"];
+    var timeInterval = d3.timeFormat("%Y-%m-%d")(minDate) + "/" + d3.timeFormat("%Y-%m-%d")(maxDate);
 
-    var map = L.map('heat_map');
+    var nextDate = new Date(minDate);
+    
+
+
+    var map = L.map('heat_map', {
+        timeDimension: true,
+        timeDimensionOptions: {
+            timeInterval: timeInterval,
+            period: "P1D"
+        },
+        timeDimensionControl: true,
+    });
+    
+    map.timeDimension.on('timeload', function(data) {
+        nextDate = new Date(data.time);
+        nextDate.setHours(0);
+        nextDate.setMinutes(0);
+        nextDate.setSeconds(0);
+        nextDate.setMilliseconds(0);
+        dateDim.filter(nextDate);
+        drawMap();
+    });
+
 
     function drawMap() {
         map.setView([52.370216, 4.895168], 7);
@@ -29,7 +57,8 @@ function makeGrGraphs(error, knmiData) {
         L.tileLayer(
             'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
                 attribution: '&copy; ' + mapLink + ' Contributors',
-                maxZoom: 15,
+                maxZoom: 14,
+
             }
 
         ).addTo(map);
@@ -38,25 +67,17 @@ function makeGrGraphs(error, knmiData) {
         _.each(allDim.top(Infinity), function(d) {
             geoData.push([d["LAT(north)"], d["LON(east)"], d["globalRadiationJcm2"]]);
         });
+
         var heat = L.heatLayer(geoData, {
-            radius: 40,
-            blur: 20,
-            maxZoom: 1,
+            radius: 50,
+            blur: 15, 
+            maxZoom: 6,
             max: maxRadiation,
+
         }).addTo(map);
     }
+
 
     drawMap();
 }
 
-// function parseGrRow(csv_row) {
-//     var d = d3.timeParse("%Y-%m-%dT")(csv_row['YYYYMMDD']);
-//     return {
-//         date: d,
-//         sunShineHour: parseFloat(csv_row['SQ']),
-//         sunShineduration: parseFloat(csv_row['SP']),
-//         globalRadiationJcm2: parseFloat(csv_row['Q']),
-//         longitudeE: (csv_row['LON(east)']),
-//         latitudeN: (csv_row['LAT(north)'])
-//     };
-// }
