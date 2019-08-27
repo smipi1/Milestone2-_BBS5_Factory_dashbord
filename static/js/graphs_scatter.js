6927164fff953ed875a2469e975f597e7edcdfd2
Yvette Smith <yvette@boesman.nl>
2019-08-27 22:31:10 +0200

    add scatterplot

--- static/js/graphs_scatter.js
+++ static/js/graphs_scatter.js
@@ -77,3 +77,38 @@ $(document).ready(function() {
             });
     }
 })
+
+// function show_phd_to_salary_correlation(ndx) {
+    
+//     var genderColors = d3.scale.ordinal()
+//         .domain(["Female", "Male"])
+//         .range(["pink", "blue"]);
+    
+//     var pDim = ndx.dimension(dc.pluck("yrs_since_phd"));
+//     var phdDim = ndx.dimension(function(d) {
+//       return [d.yrs_since_phd, d.salary, d.rank, d.sex];
+//     });
+//     var phdSalaryGroup = phdDim.group();
+    
+//     var minPhd = pDim.bottom(1)[0].yrs_since_phd;
+//     var maxPhd = pDim.top(1)[0].yrs_since_phd;
+    
+//     dc.scatterPlot("#phd-salary")
+//         .width(800)
+//         .height(400)
+//         .x(d3.scale.linear().domain([minPhd, maxPhd]))
+//         .brushOn(false)
+//         .symbolSize(8)
+//         .clipPadding(10)
+//         .xAxisLabel("Years Since PhD")
+//         .title(function(d) {
+//             return d.key[2] + " earned " + d.key[1];
+//         })
+//         .colorAccessor(function (d) {
+//             return d.key[3];
+//         })
+//         .colors(genderColors)
+//         .dimension(phdDim)
+//         .group(phdSalaryGroup)
+//         .margins({top: 10, right: 50, bottom: 75, left: 75});
+// }
