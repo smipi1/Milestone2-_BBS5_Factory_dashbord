f4ada18478fb945104a8b6ee90ee45d9661c4879
Yvette Smith <yvette@boesman.nl>
2019-08-31 19:53:39 +0200

    scater grap and moving style to CSS

--- static/js/myjs/graphs_scatter.js
+++ static/js/myjs/graphs_scatter.js
@@ -37,9 +37,10 @@ $(document).ready(function() {
 
         var joinedData = [];
         sunnyGroup.all().forEach(function(item) {
-            if(knmiLookup.hasOwnProperty(item.key) ) {
+            if (knmiLookup.hasOwnProperty(item.key)) {
                 joinedData.push({
                     day: item.key,
+                    month: new Date(item.key.getFullYear(), item.key.getMonth(), 1),
                     globalRadiationJcm2: knmiLookup[item.key],
                     kWh: item.value
                 });
@@ -52,66 +53,69 @@ $(document).ready(function() {
     function makeScGraphs(data) {
         var ndx = crossfilter(data);
         correlation_graph(ndx);
+        makeScSelector(ndx);
         dc.renderAll();
     }
 
-    // function correlation_graph(ndx) {
-
-    //     var dim = ndx.dimension(dc.pluck('globalRadiationJcm2'));
-    //     var group = dim.group(dc.pluck('kWh'));
-    //     var nonEmpty = remove_empty_bins(group);
-    //     var chart = dc.scatterPlot("#g_comparison")
-    //         .width(800)
-    //         .height(400)
-    //         .brushOn(false)
-    //         .symbolSize(8)
-    //         .clipPadding(10)
-    //         .margins({ top: 10, right: 50, bottom: 30, left: 50 })
-    //         .dimension(dim)
-    //         .group(nonEmpty)
-    //         .transitionDuration(500)
-    //         .elasticX(true)
-    //         .elasticY(true)
-    //         .yAxisLabel("kWh")
-    //         .addFilterHandler(function(filters, filter) { return [filter]; })
-    //         .x(d3.scaleTime())
-    //         .controlsUseVisibility(true)
-    //         .renderHorizontalGridLines(true)
-    //         .colors(["orange"])
-    //         .xAxisLabel(function(d) {
-    //             console.log(d);
-    //         });
-    // }
 
     function correlation_graph(ndx) {
 
-        var powerColors = d3.scaleOrdinal()
-            .domain(["kWh", "globalRadiationJcm2"])
-            .range(["red", "blue"]);
-
         var dim = ndx.dimension(function(d) {
-            return [ d.kWh, d.globalRadiationJcm2 ];
+            return [d.kWh, d.globalRadiationJcm2, d.day];
         });
         var group = dim.group();
+        var nonEmpty = remove_empty_bins(group);
 
         dc.scatterPlot("#g_comparison")
             .width(800)
             .height(400)
             .margins({ top: 10, right: 50, bottom: 75, left: 75 })
-            .x(d3.scaleLinear())
+            // .x(d3.scaleLinear())
+            .x(d3.scaleLinear().domain([0, 20]))
+            .y(d3.scaleLinear().domain([0, 20]))
             .brushOn(false)
-            // .symbolSize(8)
-            // .clipPadding(10)
-            .xAxisLabel("Month")
-            // .title(function(d) {
-            //     return d.key[2] + " earned " + d.key[1];
-            // })
-            // .colorAccessor(function(d) {
-            //     return d.key[3];
-            // })
-            // .colors(powerColors)
+            .symbolSize(8)
+            .clipPadding(10)
+            .xAxisLabel("kWh")
+            .yAxisLabel("Radiation Jcm2")
+            .renderHorizontalGridLines(true)
+            .controlsUseVisibility(true)
+            .mouseZoomable(true)
+            .addFilterHandler(function(filters, filter) { return [filter]; })
+            .elasticX(true)
+            .elasticY(true)
+            .title(function(d) {
+                return "date: " + d3.timeFormat('%x')(d.key[2]) +
+                    ", radiation: " + d.key[1] + " Jcm2" +
+                    ", energy: " + d3.format(".1f")(d.key[0]) + " kWh";
+            })
             .dimension(dim)
-            .group(group);
+            .group(nonEmpty);
     }
 
-})
+    function makeScSelector(ndx) {
+        var dim = ndx.dimension(dc.pluck("month"));
+        var group = dim.group().reduceSum(dc.pluck('day'));
+
+        var select = dc.selectMenu("#year-selector_scatter")
+            .dimension(dim)
+            .group(group);
+        select.title(function(d) {
+            return d.key;
+        });
+        return select;
+    }
+
+    function remove_empty_bins(source_group) {
+        return {
+            all: function() {
+                return source_group.all().filter(function(d) {
+                    // Filter out zero values
+                    // float is never exactly 0, so we filter out small values
+                    return Math.abs(d.value) > 0.0001;
+                });
+            }
+        };
+    }
+
+});
